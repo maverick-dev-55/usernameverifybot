80e2c334a9b64adf05e090aa68e7e4da68414b63
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-12-01 09:12:07 -0500

    added bot activity

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 var active = []
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 client.once('ready', ()=>{
+    client.user.setActivity('you', {type: "WATCHING"})
     console.log('Ready')
 })
 client.login(process.env.PASSWORD /* Bot token */)
